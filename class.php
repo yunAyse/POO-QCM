1cac5a2ce77ac1448c2c24edebdbe9e769df23f8
yunAyse <aysepc0@gmail.com>
2024-01-12 12:00:26 +0100

    files done

---
+++ class.php
@@ -0,0 +1,34 @@
+<?php 
+
+
+class Qcm {
+  private $quizz;
+
+  public function Questions () {
+
+  }
+}
+
+class Question extends Qcm {
+  private $question;
+
+  public function addQuestion () {
+
+  }
+
+}
+
+class Answer extends Question {
+  private $answer;
+
+  public function addAnswer () {
+
+  }
+
+}
+
+
+
+
+
+?>
