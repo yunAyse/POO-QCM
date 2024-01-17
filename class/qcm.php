8e62852a528fe263ceabb831f3f41624ad995c97
yunAyse <aysepc0@gmail.com>
2024-01-17 09:54:53 +0100

    commit

--- class/qcm.php
+++ class/qcm.php
@@ -9,6 +9,11 @@ class Qcm
   // an array for the questions.
   private array $questions = [];
 
+  public function __construct()
+  {
+    
+  }
+
   public function addQuestion($questions)
   {
     $this->questions = $questions;
